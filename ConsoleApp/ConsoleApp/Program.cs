aabca000f47bece38949c9acf325a43875bc40b9
Jordan Andrews <jordandrews@hotmail.com>
2020-05-04 16:08:53 +1000

    Rust and C# Libraries

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        [DllImport("RustLibrary.dll", EntryPoint = "process")]
+        private static extern void ProcessInRust();
+
+        private static void ProcessCSharp()
+        {
+            const int threadCount = 10;
+            Task[] tasks = new Task[threadCount];
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                tasks[i] = Task.Factory.StartNew(() =>
+                {
+                    int count = 0;
+                    for (int j = 0; j < 5_000_000; j++)
+                    {
+                        count += 1;
+                    }
+                    Console.WriteLine("C# Thread finished with count={0}", count);
+                });
+            }
+
+            Task.WaitAll(tasks);
+        }
+
+        static void Main(string[] args)
+        {
+            Stopwatch csharpStopwatch = new Stopwatch();
+            Stopwatch rustStopwatch = new Stopwatch();
+
+            csharpStopwatch.Start();
+            ProcessCSharp();
+            csharpStopwatch.Stop();
+
+            rustStopwatch.Start();
+            ProcessInRust();
+            rustStopwatch.Stop();
+
+            Console.WriteLine("C#: " + csharpStopwatch.ElapsedTicks);
+            Console.WriteLine("Rust: " + rustStopwatch.ElapsedTicks);
+        }
+    }
+}
